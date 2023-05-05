2b953091f5b2edb9ba8762361af211e340b80efe
Cyrborg Werewolf <alexbdelcarmen@gmail.com>
2023-05-05 12:36:00 -0600

    Add script to show/hide navbar on mobile layout.

--- script.js
+++ script.js
@@ -1,1 +1,6 @@
-const 
+const menuButton = document.querySelector(".burger-icon");
+const navbar = document.getElementById("navbar");
+
+menuButton.addEventListener("click", () => {
+  navbar.classList.toggle("hide");
+});
