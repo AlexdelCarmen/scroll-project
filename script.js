b3813065e7f98bd30e2732980d0b3f96f9a08b9f
Cyrborg Werewolf <alexbdelcarmen@gmail.com>
2023-05-06 17:00:01 -0600

    Add new logo style sections add scroll vars to JS.

--- script.js
+++ script.js
@@ -4,3 +4,9 @@ const navbar = document.getElementById("navbar");
 menuButton.addEventListener("click", () => {
   navbar.classList.toggle("hide");
 });
+
+var scrollY = window.scrollY;
+const homeButton = document.getElementById("home-button");
+const header = document.querySelector("header");
+
+console.log(header);
